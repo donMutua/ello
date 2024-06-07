1ab600016a2b483169d6ac69fd0c1d7e04121162
don.technologist <don.technologist@gmail.com>
2024-06-07 11:32:51 +0300

    chore: Add MUI theme provider

---
+++ frontend/src/Layout/index.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from "react";
+import { Box } from "@mui/material";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: {
+          xs: "column",
+          lg: "row",
+        },
+        color: "white",
+        padding: 3,
+        gap: 3,
+        overflowY: "hidden",
+        height: "100vh",
+      }}
+    >
+      <Box sx={{ width: "100%", overflowY: "scroll" }}>{children}</Box>
+    </Box>
+  );
+};
+
+export default Layout;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import App from "./App.tsx";
+import theme from "./theme.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
